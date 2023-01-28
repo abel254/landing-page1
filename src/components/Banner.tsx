fb63683d937fd85b4f541cddb4df028f48c74a9a
abel254 <abelmuema353@gmail.com>
2023-01-28 21:12:17 +0300

    responsive adjustmnet

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,29 +1,92 @@
 import { Container, Row, Col } from "react-bootstrap";
 import React from "react";
-import { Footer } from "./Footer";
 
 const banner = require("../assets/images/image.png");
+const fb = require("../assets/images/facebook_logo.png");
+const google = require("../assets/images/google-partner.png");
+const forbes = require("../assets/images/forbes.webp");
+const inc = require("../assets/images/inc.png");
+const millions = require("../assets/images/million.webp");
+const years = require("../assets/images/years.jpg");
 
 export const Banner = () => {
   return (
-    <section className="banner">
-      <Container>
-        <Row className="align-items-center">
+    <section className="banner w-100 pt-2">
+      <Container className="w-100">
+        <Row className="align-items-center mb-2 w-100" xs={1} md={2}>
           <Col md={6} lg={7}>
-            <div className="banner-text">
-              <h1>
+            <div className="banner-text w-100">
+              <h1 className="fs-1">
                 Unlock Your Business Potential With Facebook & Instagram
                 Advertising
               </h1>
               <p>Facebook Premier Level Partner Agency</p>
-              <button>WORK WITH US</button>
+              <button className="">WORK WITH US</button>
             </div>
           </Col>
           <Col md={6} lg={5} className="image">
-            <img src={banner} alt="" />
+            <img src={banner} alt="bannerImg" />
           </Col>
         </Row>
-        <Footer />
+
+        <div>
+          <Row xs={2} md={3} lg={6} className="m-auto ">
+            <Col>
+              <div className="logo">
+                <img src={fb} alt="" />
+                <p>
+                  Facebook Premier <br />
+                  Level Agencyt Partner
+                </p>
+              </div>
+            </Col>
+            <Col>
+              <div className="logo">
+                <img src={google} alt="" />
+                <p>
+                  Google Endosed <br />
+                  Marketing Partner
+                </p>
+              </div>
+            </Col>
+            <Col>
+              <div className="logo">
+                <img src={forbes} alt="" />
+                <p>
+                  Forbes Agency <br />
+                  Council Member
+                </p>
+              </div>
+            </Col>
+            <Col>
+              <div className="logo">
+                <img src={inc} alt="" />
+                <p>
+                  Inc. 5000 <br />
+                  Fastest Growing Company
+                </p>
+              </div>
+            </Col>
+            <Col>
+              <div className="logo">
+                <img src={millions} alt="" />
+                <p>
+                  In Annual <br />
+                  Digital Add Spend
+                </p>
+              </div>
+            </Col>
+            <Col>
+              <div className="logo">
+                <img src={years} alt="" />
+                <p>
+                  Years of Facebook <br />
+                  Advertising Experience
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </section>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,77 @@
 import { ImArrowUp } from "react-icons/im";
 import { ImArrowDown } from "react-icons/im";
-import {GoTriangleDown } from "react-icons/go";
+import { GoTriangleDown } from "react-icons/go";
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Navbar, Nav, NavDropdown } from "react-bootstrap";
 
-const logo = require('../assets/images/tier_logo.png')
+const logo = require("../assets/images/tier_logo.png");
 
 export const Header = () => {
   return (
     <header>
-      <Container>
-        <Row className='align-items-center'>
-          <Col md={2}>
-            <div className="logo">
-                <img src={logo} alt="" />
-            </div>
-          </Col>
-          <Col className="d-flex align-items-center justify-content-end">
-            <nav>
-              <ul>
-                <li>
-                  <a href="">WHAT WE DO <GoTriangleDown/></a>
-                  <a href="">BLOG</a>
-                  <a href="">PODCAST</a>
-                  <a href="">CAREERS</a>
-                </li>
-              </ul>
-            </nav>
-          </Col>
-          <Col md={3}>
+      <Navbar expand='md'>
+        <Container>
+          <Navbar.Brand href="#">
+            <img
+              src={logo}
+              alt="HeaderImg"
+              width="55"
+              className="d-inline-block align-top"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="toggler navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <NavDropdown title="WHAT WE DO" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link>BLOG</Nav.Link>
+              <Nav.Link>PODCAST</Nav.Link>
+              <Nav.Link>CAREERS</Nav.Link>
+            </Nav>
             <button>WORK WITH US</button>
-          </Col>
-        </Row>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar> 
+
+{/* <Navbar expand="lg">
+      <Container>
+      <Navbar.Brand href="#">
+            <img
+              src={logo}
+              alt="HeaderImg"
+              width="55"
+              className="d-inline-block align-top"
+            />
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
+    </Navbar> */}
     </header>
   );
 };
