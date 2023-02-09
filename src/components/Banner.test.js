5eb0fd2788b07b1ef5ec84767f7f4fca27c2f3eb
abel254 <abelmuema353@gmail.com>
2023-02-09 14:26:39 +0300

    added a heading test unit

--- src/components/Banner.test.js
+++ src/components/Banner.test.js
@@ -1,7 +1,9 @@
 import { Banner } from "./Banner"
 import { render, screen } from "@testing-library/react"
+import '@testing-library/jest-dom/extend-expect';
+
 test('heading should be rendered', ()=>{
     render(<Banner/>)
-    const heading = screen.getByRole('heading')
-    expect(heading).toBeInTheDocument()
+    const headingEl = screen.getByRole("heading")
+    expect(headingEl).toBeInTheDocument();
 })
